d9e2a6f40960fc93a72fde2133b7ccec492af28d
Prathamesh Deshpande <pdeshpan@iu.edu>
2019-11-14 17:07:33 -0500

    basic card layout to display info

--- City-Scanner-UI/project/src/Restaurants.js
+++ City-Scanner-UI/project/src/Restaurants.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 class Restaurants extends Component {
 
    constructor(props) {
     super(props);
+    this.state={
+      restaurants: []
+    };
   }
 
   componentDidMount()
@@ -19,7 +25,9 @@ class Restaurants extends Component {
 
     })
     .then((response)=>{
-      console.log(response)
+            console.log(response.data)
+           this.setState({restaurants: response.data.results})
+
     }).catch(err =>
       {
         console.log(err);
@@ -37,6 +45,26 @@ class Restaurants extends Component {
     return (
       <>
         <h1>Restaurants in {data}</h1>
+          <div>
+                         {
+                  this.state.restaurants.map((el,i) => (
+                    <Card key={i} style={{marginBottom: 18, width: 650, height: 150, marginRight: 18, display: 'inline-block', paddingTop: '10px', fontColor: 'black'}}>
+        
+                    <div>{el.price_level}</div> 
+                    <div name="songDetailsRec" style={{height:'inherit'}}>
+                    <div name="titleSongRec"
+                    style= {{textAlign: "center", verticalAlign: "middle", lineHeight: "140px", height:'inherit', fontWeight: "bold", fontSize: 25}}>
+                    {el.name}
+                    {el.rating}
+                    </div>
+
+                    </div>
+                
+                 
+                    </Card>))
+                  }
+          </div>
+
       </>
     );
   }
