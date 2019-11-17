3955990d34a2e405b825f7d9828ecfe1a02e7fa7
Prathamesh Deshpande <pdeshpan@iu.edu>
2019-11-17 05:12:35 -0500

    Added bootsrtap classes for styling

--- City-Scanner-UI/project/src/Homebody.js
+++ City-Scanner-UI/project/src/Homebody.js
@@ -6,8 +6,12 @@ class Homebody extends Component {
     render() {
       return (
         <>
-        <div style={{marginBottom: 200, marginTop: 100}}>
-        <div style={{marginLeft: 20, width:500, height:100}}>
+        <div className="container-fluid" style={{textAlign:'center',marginTop:50,backgroundColor:'#008B8B',height:200}}>
+        <h2 style={{top:80,position:'relative'}}>Explore CityScanner</h2>
+        </div>
+        <div className="container-fluid">
+        <div className="row">
+        <div className="col-sm-3">
           <ExampleComponent
             image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg"
             roundedColor="#321124"
@@ -16,16 +20,7 @@ class Homebody extends Component {
             roundedSize="10"
           />
         </div>
-        <div style={{marginLeft:350}}>
-        {/* <div class="box"><i class="fas fa-quote-left fa2"></i>
-            <div class="text"><i class="fas fa-quote-right fa1"></i>
-                <div>
-                    <h3>Quote the day</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
-                        book.</p>
-                </div>
-            </div>
-        </div> */}
+        <div className="col-sm-9" style={{top:85}}>
           <blockquote className="blockquote text-center" >
                     <p className="mb-0">
                     A city is a large human settlement.[4][5] It can be defined as a permanent and densely settled place with administratively defined boundaries whose members work primarily on non-agricultural tasks.[6] Cities generally have extensive systems for housing, transportation, sanitation, utilities, land use, and communication. Their density facilitates interaction between people, government organisations and businesses, sometimes benefiting different parties in the process..
@@ -36,19 +31,11 @@ class Homebody extends Component {
         </blockquote>
         </div>
         </div>
+        </div>
 
-<div style={{marginBottom: 200}}>
-<div style={{marginLeft: 1200, width:500, height:100}}>
-<ExampleComponent
-  image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg"
-  roundedColor="#321124"
-  imageWidth="300"
-  imageHeight="300"
-  roundedSize="15"
-  align="middle"
-/>
-</div>
-<div style={{marginRight:350}}>
+  <div className="container-fluid">
+        <div className="row">
+                <div className="col-sm-9" style={{top:85}}>
 <blockquote className="blockquote text-center" >
                     <p className="mb-0">
                     A city is a large human settlement.[4][5] It can be defined as a permanent and densely settled place with administratively defined boundaries whose members work primarily on non-agricultural tasks.[6] Cities generally have extensive systems for housing, transportation, sanitation, utilities, land use, and communication. Their density facilitates interaction between people, government organisations and businesses, sometimes benefiting different parties in the process..
@@ -57,34 +44,8 @@ class Homebody extends Component {
                         Someone famous in <cite title="Source Title">Source Title</cite>
                     </footer>
         </blockquote>
-</div>
-</div>
-
-<div style={{marginBottom: 200}}>
-<div style={{marginLeft: 20, width:500, height:100}}>
-          <ExampleComponent
-            image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg"
-            roundedColor="#321124"
-            imageWidth="300"
-            imageHeight="300"
-            roundedSize="15"
-            align="middle"
-          />
-        </div>
-        <div style={{marginLeft:350}}>
-        <blockquote className="blockquote text-center" >
-                    <p className="mb-0">
-                    A city is a large human settlement.[4][5] It can be defined as a permanent and densely settled place with administratively defined boundaries whose members work primarily on non-agricultural tasks.[6] Cities generally have extensive systems for housing, transportation, sanitation, utilities, land use, and communication. Their density facilitates interaction between people, government organisations and businesses, sometimes benefiting different parties in the process.
-                    </p>
-                    <footer className="blockquote-footer">
-                        Someone famous in <cite title="Source Title">Source Title</cite>
-                    </footer>
-        </blockquote>
-        </div>
-        </div>
-
-<div style={{marginBottom: 100}}>
-<div style={{marginLeft: 1200, width:500, height:100}}>
+    </div>
+        <div className="col-sm-3">
 <ExampleComponent
   image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg"
   roundedColor="#321124"
@@ -94,17 +55,57 @@ class Homebody extends Component {
   align="middle"
 />
 </div>
-<div style={{marginRight:350}}>
-<blockquote className="blockquote text-center" >
+        </div>
+        </div>
+
+<div className="container-fluid">
+        <div className="row">
+        <div className="col-sm-3">
+          <ExampleComponent
+            image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg"
+            roundedColor="#321124"
+            imageWidth="300"
+            imageHeight="300"
+            roundedSize="10"
+          />
+        </div>
+        <div className="col-sm-9" style={{top:85}}>
+          <blockquote className="blockquote text-center" >
                     <p className="mb-0">
-                    A city is a large human settlement.[4][5] It can be defined as a permanent and densely settled place with administratively defined boundaries whose members work primarily on non-agricultural tasks.[6] Cities generally have extensive systems for housing, transportation, sanitation, utilities, land use, and communication. Their density facilitates interaction between people, government organisations and businesses, sometimes benefiting different parties in the process.
+                    A city is a large human settlement.[4][5] It can be defined as a permanent and densely settled place with administratively defined boundaries whose members work primarily on non-agricultural tasks.[6] Cities generally have extensive systems for housing, transportation, sanitation, utilities, land use, and communication. Their density facilitates interaction between people, government organisations and businesses, sometimes benefiting different parties in the process..
                     </p>
                     <footer className="blockquote-footer">
                         Someone famous in <cite title="Source Title">Source Title</cite>
                     </footer>
         </blockquote>
+        </div>
+        </div>
+        </div>
+
+  <div className="container-fluid">
+        <div className="row">
+                <div className="col-sm-9" style={{top:85}}>
+<blockquote className="blockquote text-center" >
+                    <p className="mb-0">
+                    A city is a large human settlement.[4][5] It can be defined as a permanent and densely settled place with administratively defined boundaries whose members work primarily on non-agricultural tasks.[6] Cities generally have extensive systems for housing, transportation, sanitation, utilities, land use, and communication. Their density facilitates interaction between people, government organisations and businesses, sometimes benefiting different parties in the process..
+                    </p>
+                    <footer className="blockquote-footer">
+                        Someone famous in <cite title="Source Title">Source Title</cite>
+                    </footer>
+        </blockquote>
+    </div>
+        <div className="col-sm-3">
+<ExampleComponent
+  image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg"
+  roundedColor="#321124"
+  imageWidth="300"
+  imageHeight="300"
+  roundedSize="15"
+  align="middle"
+/>
 </div>
-</div>
+        </div>
+        </div>
 </>
       );
     }
