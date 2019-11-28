96295d8265aab870d088858e75a43745ebdb3c76
Virendra Wali <vwali@iu.edu>
2019-11-28 17:41:01 -0500

    Code changes

--- City-Scanner-UI/project/src/Details_housing.js
+++ City-Scanner-UI/project/src/Details_housing.js
@@ -111,7 +111,7 @@ class Details_Housing extends Component{
             console.log(err);
           });
       }
-    
+
       getShops() {
         return axios({
           method: "post",
@@ -128,7 +128,7 @@ class Details_Housing extends Component{
             console.log(err);
           });
       }
-    
+
       getBusStops() {
         return axios({
           method: "post",
@@ -145,7 +145,7 @@ class Details_Housing extends Component{
             console.log(err);
           });
       }
-    
+
       getAtms() {
         return axios({
           method: "post",
@@ -162,7 +162,7 @@ class Details_Housing extends Component{
             console.log(err);
           });
       }
-    
+
       componentDidMount() {
         this.getCoordinates();
         this.getShops();
@@ -234,29 +234,22 @@ class Details_Housing extends Component{
                 <img className="img-fluid" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg" alt="" />
                 </div>
                 <div className="col-md-4">
-                <h3 className="my-3" style={{'font-family': 'cursive'}}>About</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-                <h3 className="my-3" style={{'font-family': 'cursive'}}>Details</h3>
-                <ul>
-                    <li>Address: {ls.get("selectedIndex")["vicinity"]}</li>
-                    <li>Average Rating:{ls.get("selectedIndex")["rating"]}</li>
-                </ul>
+                    <div>
+                    <h3 className="my-3" style={{'font-family': 'cursive'}}>About</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+                    <h3 className="my-3" style={{'font-family': 'cursive'}}>Details</h3>
+                    <ul>
+                        <li>Address: {ls.get("selectedIndex")["vicinity"]}</li>
+                        <li>Average Rating:{ls.get("selectedIndex")["rating"]}</li>
+                    </ul>
+                  </div>
+
                 </div>
-              
+
             </div>
         </div>
         </div>
 
-        <div className="col-sm-6">
-              <Map
-                lat={this.state.lat}
-                long={this.state.long}
-                locations={this.state.locations}
-                shopLoc={this.state.shopLoc}
-                busStopLoc={this.state.busStopLoc}
-                atmLoc={this.state.atmLoc}
-              />
-            </div>
 
         <div className="container-fluid" style={{width:'90%',marginTop:'4%'}}>
         <div className="row">
@@ -272,7 +265,7 @@ class Details_Housing extends Component{
                         </div>
                         <div className="col-md-10">
                             <p>
-                            <a className="float-left" href="https://maniruzzaman-akash.blogspot.com/p/contact.html"><strong>Virendra Wali</strong></a>
+        <a className="float-left" href="https://maniruzzaman-akash.blogspot.com/p/contact.html"><strong>{el.username}</strong></a>
                             <span className="float-right"><i className="text-warning fa fa-star" /></span>
                             <span className="float-right"><i className="text-warning fa fa-star" /></span>
                             <span className="float-right"><i className="text-warning fa fa-star" /></span>
@@ -292,8 +285,21 @@ class Details_Housing extends Component{
           ))}
         </div>
 
+        
+        
         <div className="col-md-4">
-        <Form>
+        <div className="container">
+                  <Map
+                    lat={this.state.lat}
+                    long={this.state.long}
+                    locations={this.state.locations}
+                    shopLoc={this.state.shopLoc}
+                    busStopLoc={this.state.busStopLoc}
+                    atmLoc={this.state.atmLoc}
+                    style={{height:'80% !important'}}
+                  />
+        </div>
+        <Form style={{paddingTop:250}}>
         <fieldset className="form-group">
             <h3>Write a Post:</h3>
             <input className="form-control" type="text" id="comment" name="comment" value={this.state.comment} onChange={this.onChange}/>
@@ -310,4 +316,3 @@ class Details_Housing extends Component{
     }
 }
 export default Details_Housing;
-        
