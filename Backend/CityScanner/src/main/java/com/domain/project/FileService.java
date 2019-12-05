c6302b58c4c6441ca18294eea7f85343b59b0f5a
Prathamesh Deshpande <pdeshpan@iu.edu>
2019-12-05 17:14:01 -0500

    Created entity for events and basic functionality to add event

---
+++ Backend/CityScanner/src/main/java/com/domain/project/FileService.java
@@ -0,0 +1,38 @@
+package com.domain.project;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileService {
+
+	@Value("C:\\Users\\Hp\\Desktop\\OOSD")
+	public String uploadDir;
+
+	public void uploadFile(MultipartFile file) {
+
+		try {
+			Path copyLocation = Paths
+					.get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+			Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				throw new FileStorageException(
+						"Could not store file " + file.getOriginalFilename() + ". Please try again!");
+			} catch (FileStorageException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+}
---
+++ Backend/CityScanner/src/main/java/com/domain/project/entities/EventEntity.java
@@ -0,0 +1,132 @@
+package com.domain.project.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@Entity
+@ComponentScan
+@EntityScan
+@Table(name = "events")
+public class EventEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "eventid")
+	private String eventId;
+
+	@Column(name = "useremail")
+	private String userEmail;
+
+	@Column(name = "title")
+	private String eventTitle;
+
+	@Column(name = "description")
+	private String eventDescription;
+
+	@Column(name = "date")
+	private String eventDate;
+
+	@Column(name = "address")
+	private String eventAddress;
+
+	@Column(name = "contact")
+	private String eventContact;
+
+	@Column(name = "starttime")
+	private String eventStartTime;
+
+	@Column(name = "endtime")
+	private String eventEndTime;
+
+	@Column(name = "image")
+	private String eventImage;
+
+	public String getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(String eventId) {
+		this.eventId = eventId;
+	}
+
+
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getEventTitle() {
+		return eventTitle;
+	}
+
+	public void setEventTitle(String eventTitle) {
+		this.eventTitle = eventTitle;
+	}
+
+	public String getEventDescription() {
+		return eventDescription;
+	}
+
+	public void setEventDescription(String eventDescription) {
+		this.eventDescription = eventDescription;
+	}
+
+	public String getEventDate() {
+		return eventDate;
+	}
+
+	public void setEventDate(String eventDate) {
+		this.eventDate = eventDate;
+	}
+
+	public String getEventAddress() {
+		return eventAddress;
+	}
+
+	public void setEventAddress(String eventAddress) {
+		this.eventAddress = eventAddress;
+	}
+
+	public String getEventContact() {
+		return eventContact;
+	}
+
+	public void setEventContact(String eventContact) {
+		this.eventContact = eventContact;
+	}
+
+	public String getEventStartTime() {
+		return eventStartTime;
+	}
+
+	public void setEventStartTime(String eventStartTime) {
+		this.eventStartTime = eventStartTime;
+	}
+
+	public String getEventEndTime() {
+		return eventEndTime;
+	}
+
+	public void setEventEndTime(String eventEndTime) {
+		this.eventEndTime = eventEndTime;
+	}
+
+	public String getEventImage() {
+		return eventImage;
+	}
+
+	public void setEventImage(String eventImage) {
+		this.eventImage = eventImage;
+	}
+
+}
