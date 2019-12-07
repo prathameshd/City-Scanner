b9e06711ddec21331f541e291c7d9b6a3943a698
Virendra Wali <vwali@iu.edu>
2019-12-07 15:30:12 -0500

    Adding Restaurant landing page, details page and features on details page
    https://test.uisapp2.iu.edu/jira-stg/browse/P532F19T11-83
    
    https://test.uisapp2.iu.edu/jira-stg/browse/P532F19T11-84
    
    https://test.uisapp2.iu.edu/jira-stg/browse/P532F19T11-85
    
    https://test.uisapp2.iu.edu/jira-stg/browse/P532F19T11-86
    
    https://test.uisapp2.iu.edu/jira-stg/browse/P532F19T11-87

--- Backend/CityScanner/src/main/java/com/domain/project/repositories/UserRestaurantPostRepositoy.java
+++ Backend/CityScanner/src/main/java/com/domain/project/repositories/UserRestaurantPostRepositoy.java
@@ -4,11 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Repository;
 
-
+import com.domain.project.entities.UserHousingPostEntity;
 import com.domain.project.entities.UserRestaurantPostEntity;
 
 @Repository
@@ -17,5 +18,8 @@ public interface UserRestaurantPostRepositoy extends JpaRepository<UserRestauran
 	
 	@Query("SELECT u FROM UserRestaurantPostEntity u WHERE postsubjectname = ?1")
 	List<UserRestaurantPostEntity> findByTitle(String postsubjectname);
+	
+	@Query("SELECT u FROM UserRestaurantPostEntity u WHERE postid = ?1")
+	Optional<UserRestaurantPostEntity> findById(String postid);
 	
 }
